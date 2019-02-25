dfdd23b6f77f6928a38f56c7490ea37d04dab385
PaurakhSharma <paurakh011@gmail.com>
2019-02-25 22:52:21 +0545

    create new component for main navigation and apply DRY 😎

---
+++ components/MainNavigation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="flex justify-between items-center">
+      <h1 class="pt-8 mb-8 text-2xl">
+        <nuxt-link to="/" class="text-black no-underline hover:text-grey-darkest">My simple blog</nuxt-link>
+      </h1>
+      <div class="flex">
+        <div class="mr-6">
+          <nuxt-link class="nav text-black no-underline" to="/about">About</nuxt-link>
+        </div>
+        <div>
+          <nuxt-link class="nav text-black no-underline" to="/team">Team</nuxt-link>
+        </div>
+      </div>
+    </div>
+    <div class="border-b border-solid border-grey mb-8"></div>
+  </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class='container font-sans text-black'>
-    <div class='flex justify-between items-center'>
-      <h1 class="pt-8 mb-8 text-2xl">
-        <nuxt-link to="/" class="text-black no-underline hover:text-grey-darkest">My simple blog</nuxt-link>
-      </h1>
-      <div class='flex'>
-        <div class="mr-6"><nuxt-link class="nav text-black no-underline" to="/about">About</nuxt-link></div>
-        <div><nuxt-link class="nav text-black no-underline" to="/team">Team</nuxt-link></div>
-      </div>
-    </div>
-    <div class="border-b border-solid border-grey mb-8"></div>
-    <nuxt />
+  <div class="container font-sans text-black">
+    <main-navigation></main-navigation>
+    <nuxt/>
   </div>
 </template>
 
+<script>
+import MainNavigation from '../components/MainNavigation'
+
+export default {
+  components: {
+    MainNavigation
+  }
+}
+</script>
+
 
 <style>
- .container-inner {
-   max-width: 650px;
- }
+.container-inner {
+  max-width: 650px;
+}
 
- .nav.nuxt-link-active {
-   font-weight: bold;
-   text-decoration: underline;
- }
+.nav.nuxt-link-active {
+  font-weight: bold;
+  text-decoration: underline;
+}
 </style>
 
