26b7694bec07e19764ca8b86e371d732f836487f
PaurakhSharma <paurakh011@gmail.com>
2019-02-25 22:40:41 +0545

    Make it eye friendly 🥰

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,28 @@
 <template>
   <div class='container font-sans text-black'>
     <div class='flex justify-between items-center'>
-      <h1><nuxt-link to="/">My simple blog</nuxt-link></h1>
+      <h1 class="pt-8 mb-8 text-2xl">
+        <nuxt-link to="/" class="text-black no-underline hover:text-grey-darkest">My simple blog</nuxt-link>
+      </h1>
       <div class='flex'>
-        <div><nuxt-link to="/about">About</nuxt-link></div>
-        <div><nuxt-link to="/team">Team</nuxt-link></div>
+        <div class="mr-6"><nuxt-link class="nav text-black no-underline" to="/about">About</nuxt-link></div>
+        <div><nuxt-link class="nav text-black no-underline" to="/team">Team</nuxt-link></div>
       </div>
     </div>
+    <div class="border-b border-solid border-grey mb-8"></div>
     <nuxt />
   </div>
 </template>
+
+
+<style>
+ .container-inner {
+   max-width: 650px;
+ }
+
+ .nav.nuxt-link-active {
+   font-weight: bold;
+   text-decoration: underline;
+ }
+</style>
+
--- pages/blog/blog-post-4.vue
+++ pages/blog/blog-post-4.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    Blog post 4
+  <div class="container-inner m-auto leading-normal">
+    <h1 class="mb-4">My blog post 4</h1>
+    <p class="mb-4 text-lg">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio nobis numquam odit distinctio facilis exercitationem minima quas quis adipisci beatae consequatur asperiores, at facere harum! Consectetur maxime omnis ut voluptas quisquam culpa. Non, modi tempore neque in laborum eum laboriosam vero fugiat beatae mollitia, tempora a, hic amet sit. In!
+    </p>
+    <p class="mb-4 text-lg">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio nobis numquam odit distinctio facilis exercitationem minima quas quis adipisci beatae consequatur asperiores, at facere harum! Consectetur maxime omnis ut voluptas quisquam culpa. Non, modi tempore neque in laborum eum laboriosam vero fugiat beatae mollitia, tempora a, hic amet sit. In!
+    </p>
+    <p class="mb-4 text-lg">
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio nobis numquam odit distinctio facilis exercitationem minima quas quis adipisci beatae consequatur asperiores, at facere harum! Consectetur maxime omnis ut voluptas quisquam culpa. Non, modi tempore neque in laborum eum laboriosam vero fugiat beatae mollitia, tempora a, hic amet sit. In!
+    </p>
   </div>
 </template>
